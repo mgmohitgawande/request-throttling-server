3873204bd9ec002dea0233a9d22001f79812cc87
Mohit <mgmohitgawande@gmail.com>
2018-04-26 16:01:34 +0530

    saperate bottlenecks for different marketerers of outbrain

--- routes/router.js
+++ routes/router.js
@@ -4,11 +4,17 @@ module.exports = function(app){
     var proxy = require('express-http-proxy');
     var Bottleneck = require("bottleneck");
     
-    var outbrainLimiter = new Bottleneck({maxConcurrent: 5, minTime: 12*1000});
+    var outbrainLimiterFactory = function(){
+        var outbrainLimiterIdentifireMap = {}
+        return function(identifire){
+            return outbrainLimiterIdentifireMap[identifire] ? outbrainLimiterIdentifireMap[identifire] : (outbrainLimiterIdentifireMap[identifire] = new Bottleneck({maxConcurrent: 5, minTime: 12*1000}))
+        }
+    }
+    var outbrainLimiter = outbrainLimiterFactory()
     app.use(function(req, res, next){
-	console.log(new Date(), 'logging req', req.body, req.headers, req.url)
+        console.log(new Date(), 'logging req', req.body, req.headers, req.url)
     })
-    app.use('/outbrain', (req, res, next) => outbrainLimiter.submit((n) => n(), next), proxy('https://api.outbrain.com'))
+    app.use('/outbrain', (req, res, next) => outbrainLimiter(req.url.split('/')[6]).submit((n) => n(), next), proxy('https://api.outbrain.com'))
     
     app.post('/test', function(req, res){
         console.log('hit test')
